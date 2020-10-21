8cd4782ef959214c9db383484ce36890cbfd1cb0
daddiboi <sahilpreet907@gamil.com>
2020-10-21 20:16:35 +0530

    senpai

---
+++ app/src/main/java/com/infinitysensei/kaguyawallpapersama/Activity24.java
@@ -0,0 +1,14 @@
+package com.infinitysensei.kaguyawallpapersama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity24 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_24);
+    }
+}

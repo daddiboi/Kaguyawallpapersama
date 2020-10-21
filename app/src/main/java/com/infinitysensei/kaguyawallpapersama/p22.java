88f7af7585eda3a8f0aef9e127de6d08b85997f4
daddiboi <sahilpreet907@gamil.com>
2020-10-21 20:45:16 +0530

    senpai

--- app/src/main/java/com/infinitysensei/kaguyawallpapersama/p22.java
+++ app/src/main/java/com/infinitysensei/kaguyawallpapersama/p22.java
@@ -3,12 +3,21 @@ package com.infinitysensei.kaguyawallpapersama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class p22 extends AppCompatActivity {
-
+    ImageButton button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.p22);
+        button1=(ImageButton)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
